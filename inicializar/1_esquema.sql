4cb4463cb8ddcb2f80d329c6d5a657f1fe503978
Mateus Niedusiak <mat23brutinho@gmail.com>
2026-06-27 20:31:03 -0400

    esquema e inserts

--- inicializar/1_esquema.sql
+++ inicializar/1_esquema.sql
@@ -19,7 +19,7 @@ CREATE TABLE Local (
 CREATE TABLE Prepper (
     id_prepper INT PRIMARY KEY,
     nome VARCHAR(50),
-    estrelas_conexao INT,
+    estrelas_conexao INT CHECK (estrelas_conexao BETWEEN 0 AND 5),
     id_local INT UNIQUE,
     FOREIGN KEY (id_local) REFERENCES Local(id_local)
 );
@@ -27,22 +27,22 @@ CREATE TABLE Prepper (
 CREATE TABLE Portador (
     id_portador INT PRIMARY KEY,
     nome VARCHAR(50),
-    capacidade_max_kg DECIMAL(5,2),
-    total_likes INT
+    capacidade_max_kg DECIMAL(5,2) CHECK (capacidade_max_kg > 0),
+    total_likes INT CHECK (total_likes >= 0)
 );
 
 CREATE TABLE Item (
     id_item INT PRIMARY KEY,
     nome VARCHAR(50),
     categoria VARCHAR(50),
-    peso_kg DECIMAL(5,2),
+    peso_kg DECIMAL(5,2) CHECK (peso_kg > 0),
     nivel_fragilidade VARCHAR(50)
 );
 
 CREATE TABLE Pedido (
     id_pedido INT PRIMARY KEY,
-    status_pedido VARCHAR(50),
-    condicao_final_carga VARCHAR(50),
+    status_pedido VARCHAR(50) CHECK (status_pedido IN ('Pendente','Em Transporte','Entregue','Cancelado')),
+    condicao_final_carga VARCHAR(50) CHECK (condicao_final_carga IN ('Perfeita','Danificada','Destruida')),
     id_local_origem INT,
     id_local_destino INT,
     id_portador INT,
@@ -54,7 +54,7 @@ CREATE TABLE Pedido (
 CREATE TABLE Pedido_Item (
     id_pedido INT,
     id_item INT,
-    quantidade INT,
+    quantidade INT CHECK (quantidade > 0),
     PRIMARY KEY (id_pedido, id_item),
     FOREIGN KEY (id_pedido) REFERENCES Pedido(id_pedido),
     FOREIGN KEY (id_item) REFERENCES Item(id_item)
@@ -63,7 +63,7 @@ CREATE TABLE Pedido_Item (
 CREATE TABLE Estrutura (
     id_estrutura INT PRIMARY KEY,
     tipo VARCHAR(50),
-    durabilidade_porcentual DECIMAL(5,2),
+    durabilidade_porcentual DECIMAL(5,2) CHECK (durabilidade_porcentual BETWEEN 0 AND 100),
     id_portador INT,
     id_area INT,
     FOREIGN KEY (id_portador) REFERENCES Portador(id_portador),
@@ -72,7 +72,7 @@ CREATE TABLE Estrutura (
 
 CREATE TABLE Avaliacao_like (
     id_like INT PRIMARY KEY,
-    quantidade INT,
+    quantidade INT CHECK (quantidade >= 0),
     motivo VARCHAR(50),
     origem_like VARCHAR(50),
     id_portador INT,
